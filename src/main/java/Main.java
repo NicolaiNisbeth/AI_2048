bd4af5462c14965c1a2b01d4ff035eb9c456a727
jdkramhoft <s175445@student.dtu.dk>
2020-03-10 09:41:35 +0100

    Initial state

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[][] board = new int[4][4];
+        for(int[] row : board)
+            Arrays.fill(row, -1);
+        int startLocation = (int) (Math.random() * 16);
+        board[startLocation/4][startLocation%4] = 2;
+        State initialState = new State(board);
     }
 }
