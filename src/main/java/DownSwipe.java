a66c950b9e173c81b1990c9547b0bc92e564ef8c
jdkramhoft <s175445@student.dtu.dk>
2020-03-10 10:38:48 +0100

    Copy board

--- src/main/java/DownSwipe.java
+++ src/main/java/DownSwipe.java
@@ -1,7 +1,22 @@
 public class DownSwipe implements Action {
     @Override
     public State getResult(State state) {
-        return null;
+        int[][] board = Utils.copyBoard(state);
+        int head = 0;
+        for (int j = 0; j < board.length; j++) {
+            for (int i = 0; i < board[j].length; i++) {
+                if(board[i][j] != -1){
+                    if(head == 0){
+                        board[head++][j] = board[i][j];
+                    } else {
+                        if(board[head-1][j] == board[i][j]){
+                            board[head-1][j] += board[i][j];
+                        }
+                    }
+                }
+            }
+        }
+        return new State(board);
     }
 
     @Override
---
+++ src/main/java/Utils.java
@@ -0,0 +1,10 @@
+public class Utils {
+    static int[][] copyBoard(State state){
+        int[][] board = state.getBoard();
+        int[][] copy = new int[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            System.arraycopy(board[i], 0, copy[i], 0, board[i].length);
+        }
+        return copy;
+    }
+}
