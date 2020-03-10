ed27d4d11dbaf32549d8214463ef598ad3b640a3
jdkramhoft <s175445@student.dtu.dk>
2020-03-10 11:08:01 +0100

    Ready for unit test

--- src/main/java/DownSwipe.java
+++ src/main/java/DownSwipe.java
@@ -1,22 +1,51 @@
+import com.sun.tools.javac.util.Pair;
+
+import java.util.ArrayList;
+
 public class DownSwipe implements Action {
+
+    private ArrayList<Pair<Integer, Integer>> emptySquares = new ArrayList<>();
+
     @Override
     public State getResult(State state) {
         int[][] board = Utils.copyBoard(state);
         for (int j = 0; j < board.length; j++) {
-            int head = 0;
+            int emptySquareCount = 0;
+            int head = board[j].length-1;
             for (int i = 0; i < board[j].length; i++) {
                 if(board[i][j] != -1){
-                    if(head == 0){
-                        board[head++][j] = board[i][j];
+                    if(head == board[j].length-1){
+                        board[head--][j] = board[i][j];
                     } else {
-                        if(board[head-1][j] == board[i][j]){
-                            board[head-1][j] += board[i][j];
+                        if(board[head+1][j] == board[i][j]){
+                            board[head+1][j] += board[i][j];
+                            emptySquareCount++;
                         }
                     }
+                } else {
+                    emptySquareCount++;
+                }
+            }
+            boolean lastIteration = j == board.length -1;
+            if(lastIteration){
+                for (int i = 0; i < emptySquareCount; i++) {
+                    emptySquares.add(new Pair<>(i,j));
                 }
             }
         }
+        spawn(board);
         return new State(board);
+    }
+
+    private void spawn(int[][] board) {
+        if(emptySquares.size() == 0)
+            return;
+
+        int count = emptySquares.size();
+        int random = (int) (Math.random() * count);
+        Pair<Integer, Integer> coordinates = emptySquares.get(random);
+        int valueToSpawn = (Math.random() > 0.9) ? 4 : 2;
+        board[coordinates.fst][coordinates.snd] = valueToSpawn;
     }
 
     @Override
